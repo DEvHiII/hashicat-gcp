2a47bd0b40aab03d4996cc9980e4b9567bcfe661
Dominic <terraform@r.iioi.eu>
2021-11-03 18:21:30 +0000

    againagain

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "network" {
   source  = "app.terraform.io/terraformtraining202111/network/google"
   version = "3.4.0"
   project_id   = var.project
-  network_name = "gaurav-network"
+  network_name = "gaurav-network2"
     subnets = [
     {
       subnet_name   = "gaurav-subnet"
